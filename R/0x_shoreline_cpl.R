77012336872de02ef94977d8bf08aefdfd7b6e8a
isakro <isak.hkr@hotmail.com>
2023-08-15 19:54:17 +0200

    Setting up supp 2

--- R/0x_shoreline_cpl.R
+++ R/0x_shoreline_cpl.R
@@ -13,22 +13,17 @@ load(file = here::here("analysis/data/derived_data/shore_models_pd.RData"))
 cpl_1 <- JDEoptim(lower = 0, upper = 1, fn = objectiveFunction, PDarray = pd,
                   type = 'CPL', NP = 20, trace = TRUE)
 cpl_2 <- JDEoptim(lower = rep(0, 3), upper = rep(1, 3), fn = objectiveFunction,
-                  PDarray = pd, type = 'CPL', NP = 60,  maxiter = 400 * 3,
-                  trace = TRUE)
+                  PDarray = pd, type = 'CPL', NP = 60, trace = TRUE)
 cpl_3 <- JDEoptim(lower = rep(0, 5), upper = rep(1, 5), fn = objectiveFunction,
-                  PDarray = pd, type = 'CPL', NP = 100, maxiter = 400 * 10,
-                  trace = TRUE)
+                  PDarray = pd, type = 'CPL', NP = 100, trace = TRUE)
 cpl_4 <- JDEoptim(lower = rep(0, 7), upper = rep(1, 7), fn = objectiveFunction,
-                  PDarray = pd, type = 'CPL', NP = 140, maxiter = 400 * 10,
-                  trace = TRUE)
+                  PDarray = pd, type = 'CPL', NP = 140, trace = TRUE)
 cpl_5 <- JDEoptim(lower = rep(0, 9), upper = rep(1, 9), fn = objectiveFunction,
-                  PDarray = pd, type='CPL', NP = 180, maxiter = 400 * 10,
-                  trace = TRUE)
+                  PDarray = pd, type='CPL', NP = 180, trace = TRUE)
 cpl_6 <- JDEoptim(lower = rep(0, 11), upper = rep(1,11), fn = objectiveFunction,
-                  PDarray = pd, type = 'CPL', NP = 220, maxiter =  400 * 11,
-                  trace = TRUE)
+                  PDarray = pd, type = 'CPL', NP = 220, trace = TRUE)
 
-save(cpl_1, cpl_2, cpl_3, cpl_4,
+save(cpl_1, cpl_2, cpl_3, cpl_4, cpl_5, cpl_6,
      file = here::here("analysis/data/derived_data/shore_models.RData"))
 load(file = here::here("analysis/data/derived_data/shore_models.RData"))
 
@@ -41,7 +36,7 @@ cpl3$model <- "3-CPL"
 cpl4 <- convertPars(pars = cpl_4$par, years = minage:maxage, type = 'CPL')
 cpl4$model <- "4-CPL"
 
-shmodels <- rbind(expp, logip, unifp, cpl1, cpl2, cpl3, cpl4)
+shmodels <- rbind(expp, logip, unifp, cpl1, cpl2, cpl3, cpl4, cpl5, cpl6)
 
 sh_lik <- c("Exponential" = -exp$value,
                  "Logistic" = -logi$value,
@@ -49,16 +44,20 @@ sh_lik <- c("Exponential" = -exp$value,
                  "1-CPL" = -cpl_1$value,
                  "2-CPL" = -cpl_2$value,
                  "3-CPL" = -cpl_3$value,
-                 "4-CPL" = -cpl_4$value)
+                 "4-CPL" = -cpl_4$value,
+                 "5-CPL" = -cpl_5$value,
+                 "6-CPL" = -cpl_6$value)
 
 ssize <- ncol(pd)
 sh_bic <- c(log(ssize)*1 - 2*sh_lik[1], # Exponential
                  log(ssize)*2 - 2*sh_lik[2], # Logistic
                  log(ssize)*0  - 2*sh_lik[3], # Uniform
-                 log(ssize)*1 - 2*sh_lik[4], # CPL-1
-                 log(ssize)*3 - 2*sh_lik[5],
-                 log(ssize)*5 - 2*sh_lik[6],
-                 log(ssize)*7 - 2*sh_lik[7])
+                 log(ssize)*1 - 2*sh_lik[4], # 1-CPL
+                 log(ssize)*3 - 2*sh_lik[5], # 2-CPL
+                 log(ssize)*5 - 2*sh_lik[6], # 3-CPL
+                 log(ssize)*7 - 2*sh_lik[7], # 4-CPL
+                 log(ssize)*9 - 2*sh_lik[8], # 5-CPL
+                 log(ssize)*11 - 2*sh_lik[9]) # 6-CPL
 
 SPD <- as.data.frame(rowSums(pd))
 SPD <- SPD/(sum(SPD) * 5)
@@ -82,8 +81,7 @@ bicplt_shore <- ggplot() +
   theme_bw() +
   theme(panel.border = element_rect(linewidth = 0.5))
 
-bicplt_shore + bicplt
-cplplt + bicplt
+cplplt + bicplt_shore
 
 # Save pd, 4-CPL model and min and max age for mcmc
 save(cpl_4, pd, minage, maxage,
