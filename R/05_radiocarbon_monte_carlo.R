34806de24eb43c78b64861507c55f172c50ce67f
isakro <isak.hkr@hotmail.com>
2023-12-31 03:04:35 +0100

    Final edits before re-submission for PCI

--- R/05_radiocarbon_monte_carlo.R
+++ R/05_radiocarbon_monte_carlo.R
@@ -1,5 +1,8 @@
 # This script performs Monte Carlo simulation using the radiocarbon data and
 # plots results from both simulations with both shoreline and radiocarbon dates.
+# Based on the failed rejection of the logistic model, a version of the model
+# fit when accounting for taphonomy is then created (figure included in
+# as supplementary material)
 
 library(dplyr)
 library(rcarbon)
@@ -24,6 +27,7 @@ c14spd <- spd(caldates, timeRange = c(12000, 4500))
 minage <- min(c14spd$grid[c14spd$grid$PrDens > 0,]$calBP)
 maxage <- max(c14spd$grid[c14spd$grid$PrDens > 0,]$calBP)
 
+# c14spd_taph <- transformSPD(c14spd)
 
 # Switch to ADMUR
 names(c14) <- c("site", "context", "material", "lab_ref", "age", "sd")
@@ -59,6 +63,7 @@ plotSimulationSummary(expsum)
 plotSimulationSummary(logsum)
 plotSimulationSummary(unisum)
 
+
 # Plotting using custom plot function
 expplotr <- plot_mc(expsum)
 logplotr <- plot_mc(logsum)
@@ -75,3 +80,71 @@ load(here("analysis/data/derived_data/uniplts.rda"))
 
 ggsave(here::here("analysis/figures/mc.png"),
        units = "px", width = 4000, height = 1250*2)
+
+
+#### Logistic model with taphonomic loss ####
+
+# Use the default parameter settings from ADMUR, as there are no available
+# prior data on which to adjust these from the region (see the ADMUR vignette)
+logtaph <- JDEoptim(lower = c(0, 0000, 0, -3), upper = c(1, 10000, 20000,0),
+                    fn = objectiveFunction, PDarray = PD, type = 'logistic',
+                    NP = 40, taphonomy = TRUE)
+
+# Retrieve the logistic model fit without accoutning for taphonomy
+pop_log <- convertPars(pars=log$par,
+                       years=minage:maxage,
+                       type='logistic')
+
+# Retrieve the population dynamics component of the logistic model fit when
+# accounting for taphonomy
+pop_log_taph <- convertPars(pars=logtaph$par[1:2],
+                            years=minage:maxage,
+                            type='logistic')
+
+# Retrieve the taphonomic component
+taph_log_taph <- convertPars(pars=logtaph$par[3:4],
+                            years=minage:maxage,
+                            type='power')
+
+# Assemble the SPD
+SPD <- as.data.frame(rowSums(PD))
+SPD <- SPD/( sum(SPD) *5 )
+
+popplt <- ggplot() +
+  geom_line(data = SPD, aes(x = as.numeric(rownames(SPD)), SPD[,1])) +
+  geom_line(data = pop_log, aes(x = year, pdf, col = "pop")) +
+  geom_line(data = pop_log_taph, aes(x = year, y = pdf, col = "poptaph")) +
+  labs(x = "BCE", y = "Summed probability",
+       title = "Population dynamics component") +
+  scale_colour_manual(name = "",
+                      labels = c("Logistic model with taphonomy",
+                                 "Logistic model"),
+                      breaks = c("poptaph", "pop"),
+                      values = c("red", "blue")) +
+  scale_x_reverse(limits = c(11950, 4450),
+                  breaks = seq(11950, 4450, -1000),
+                  expand = expansion(mult = c(0, 0)),
+                  labels = function(x)(x-1950)*-1) +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.1)),
+                     labels = scales::label_comma()) +
+  theme_bw() +
+  theme(legend.position = "bottom")
+
+taphplt <- ggplot() +
+  geom_line(data = SPD, aes(x = as.numeric(rownames(SPD)), SPD[,1]), col = NA) +
+  geom_line(data = taph_log_taph, aes(x = year, pdf), col = "green4") +
+  labs(x = "BCE", y = "Probability distribution",
+       title = "Taphonomic component") +
+  scale_x_reverse(limits = c(11950, 4450),
+                  breaks = seq(11950, 4450, -1000),
+                  expand = expansion(mult = c(0, 0)),
+                  labels = function(x)(x-1950)*-1) +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.1)),
+                     labels = scales::label_comma()) +
+  theme_bw()
+
+popplt + taphplt +
+  plot_annotation(tag_levels = 'A')
+
+ggsave(here::here("analysis/figures/taphonomic.png"),
+       units = "px", width = 3000, height = 1500)
